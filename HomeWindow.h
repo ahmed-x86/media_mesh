e2f185e8ce0d5ee1c48538ff83272664e503ab8c
ahmed-x86 <ahmdansmr7@gmail.com>
2026-08-01 12:35:52 +0300

    Add HomeWindow.h

--- HomeWindow.h
+++ HomeWindow.h
@@ -4,6 +4,10 @@
 #include <QTimer>
 #include <QLineEdit>
 #include <QComboBox>
+#include <QStackedWidget>
+#include <QPushButton>
+
+class RippleButton;
 
 class HomeWindow : public QWidget {
     Q_OBJECT
@@ -13,13 +17,34 @@ public:
 protected:
     void paintEvent(QPaintEvent* event) override;
     bool eventFilter(QObject* watched, QEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
 
 private:
+    // Background Glow
     QPointF m_currentGlowPos, m_targetGlowPos;
     QTimer* m_glowTimer;
+
+    // Converter UI Elements
     QLineEdit *m_pathInput;
     QComboBox *m_profileCombo;
 
+    // Sidebar & Navigation Layout
+    bool m_isSidebarOpen = false;
+    QWidget *m_sidebar;
+    QWidget *m_mainContent;
+    QWidget *m_topBar;
+    QStackedWidget *m_stackedWidget;
+    QWidget *m_homePage, *m_converterPage, *m_installPage;
+    QPushButton *m_hamburgerBtn;
+    
+    // Navigation Buttons
+    RippleButton *m_btnNavHome, *m_btnNavConvert, *m_btnNavInstall;
+
     void applyStyle();
     void setupUI();
+    void buildSidebar();
+    void toggleSidebar();
+    
+    // Installer Helper
+    void installExtension(const QString &fmName, const QString &destPath, const QString &restartCmd);
 };
