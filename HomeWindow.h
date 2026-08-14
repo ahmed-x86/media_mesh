2379067ca34a310c419fd65eaf99d5d0d6d78c3f
ahmed-x86 <ahmdansmr7@gmail.com>
2026-08-14 18:18:58 +0300

    Add HomeWindow.h

--- HomeWindow.h
+++ HomeWindow.h
@@ -20,15 +20,13 @@ protected:
     void resizeEvent(QResizeEvent* event) override;
 
 private:
-    // Background Glow
     QPointF m_currentGlowPos, m_targetGlowPos;
     QTimer* m_glowTimer;
 
-    // Converter UI Elements
     QLineEdit *m_pathInput;
     QComboBox *m_profileCombo;
+    QComboBox *m_hwAccelCombo;
 
-    // Sidebar & Navigation Layout
     bool m_isSidebarOpen = false;
     QWidget *m_sidebar;
     QWidget *m_mainContent;
@@ -37,14 +35,11 @@ private:
     QWidget *m_homePage, *m_converterPage, *m_installPage;
     QPushButton *m_hamburgerBtn;
     
-    // Navigation Buttons
     RippleButton *m_btnNavHome, *m_btnNavConvert, *m_btnNavInstall;
 
     void applyStyle();
     void setupUI();
     void buildSidebar();
     void toggleSidebar();
-    
-    // Installer Helper
     void installExtension(const QString &fmName, const QString &destPath, const QString &restartCmd);
 };
